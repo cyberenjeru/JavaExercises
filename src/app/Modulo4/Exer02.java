dda467aceed88ae7f3ea59c3c8e4d4ec90e79416
cyberenjeru <viniciusbernardesxcx@gmail.com>
2023-07-18 13:45:52 -0300

    exercicios java ifrs

--- src/app/Modulo4/Exer02.java
+++ src/app/Modulo4/Exer02.java
@@ -2,14 +2,14 @@ package app.Modulo4;
 
 import java.util.Scanner;
 
-public class Exer01 {
+public class Exer02 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Entre com o primeiro número: ");
         double firstNum = scan.nextDouble();
         System.out.println("Entre com o segundo número: ");
         double secondNum = scan.nextDouble();
-        double resultado = firstNum - secondNum;
-        System.out.println("O resultado da subtração entre o primeiro e o segundo número é: " + resultado);
+        double result = firstNum / secondNum;
+        System.out.println("O resultado da divisão entre o primeiro e o segundo número é: " + result);
     }
 }
